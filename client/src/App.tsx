bf6c483a29530940532f0b127aa4358a960276d9
Carlnewton-star <mwanikicaleb0@gmail.com>
2026-09-24 09:53:55 +0300

    Add admin routes and components to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,13 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Account from "./pages/Account";
 import NotFound from "./pages/NotFound";
+import AdminRoute from "./components/AdminRoute";
+import AdminLayout from "./components/AdminLayout";
+import AdminDashboard from "./pages/admin/Dashboard";
+import AdminProducts from "./pages/admin/Products";
+import AdminOrders from "./pages/admin/Orders";
+import AdminStock from "./pages/admin/Stock";
+import AdminBlog from "./pages/admin/Blog";
 
 export default function App() {
   return (
@@ -21,6 +28,20 @@ export default function App() {
         <Route path="register" element={<Register />} />
         <Route path="account" element={<Account />} />
         <Route path="*" element={<NotFound />} />
+      </Route>
+      <Route
+        path="admin"
+        element={
+          <AdminRoute>
+            <AdminLayout />
+          </AdminRoute>
+        }
+      >
+        <Route index element={<AdminDashboard />} />
+        <Route path="products" element={<AdminProducts />} />
+        <Route path="orders" element={<AdminOrders />} />
+        <Route path="stock" element={<AdminStock />} />
+        <Route path="blog" element={<AdminBlog />} />
       </Route>
     </Routes>
   );
